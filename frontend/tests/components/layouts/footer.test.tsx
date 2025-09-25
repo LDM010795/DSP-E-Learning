79f7ec0467c326cfb1a2db410ccf92c2921a0c45
AndreasDiller <AndreasDiller@AndreasDiller.onmicrosoft.com>
2025-09-25 10:16:10 +0200

    footer test

---
+++ frontend/tests/components/layouts/footer.test.tsx
@@ -0,0 +1,33 @@
+import { describe } from "vitest";
+import FooterNavigation from "../../../src/components/layouts/footer";
+import { screen } from "@testing-library/react";
+import { renderWithAppProviders } from "../../test-utils";
+
+describe("FooterNavigation", () => {
+    it("Copyright is displayed", () => {
+        renderWithAppProviders(<FooterNavigation />)
+        const currentYear = new Date().getFullYear()
+        expect(screen.getByText(`© ${currentYear} DataSmart Learning`)).toBeInTheDocument();
+    })
+
+    it("Impressum Link is displayed and works", () => {
+        renderWithAppProviders(<FooterNavigation />)
+        const impressumLink = screen.getByRole("link", { name: "Impressum" });
+        expect(impressumLink).toBeInTheDocument();
+        expect(impressumLink).toHaveAttribute("href", "/Impressum");
+    })
+
+    it("Datenschutz is displayed", () => {
+        renderWithAppProviders(<FooterNavigation />)
+        const datenschutzLink = screen.getByRole("link", { name: "Datenschutz" });
+        expect(datenschutzLink).toBeInTheDocument();
+        expect(datenschutzLink).toHaveAttribute("href", "/Datenschutz");
+    })
+
+    it("AGBs are displayed", () => {
+        renderWithAppProviders(<FooterNavigation />)
+        const agbLink = screen.getByRole("link", { name: "AGBs und Widerrufsbelehrung" });
+        expect(agbLink).toBeInTheDocument();
+        expect(agbLink).toHaveAttribute("href", "/AGB");
+    })
+})
