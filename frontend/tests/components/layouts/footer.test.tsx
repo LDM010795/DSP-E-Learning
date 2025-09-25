9d6022df5da543357df4840671334729bcbb8839
AndreasDiller <220184647+AndreasDiller@users.noreply.github.com>
2025-09-25 10:15:35 +0000

    Apply automatic changes

--- frontend/tests/components/layouts/footer.test.tsx
+++ frontend/tests/components/layouts/footer.test.tsx
@@ -4,30 +4,34 @@ import { screen } from "@testing-library/react";
 import { renderWithAppProviders } from "../../test-utils";
 
 describe("FooterNavigation", () => {
-    it("Copyright is displayed", () => {
-        renderWithAppProviders(<FooterNavigation />)
-        const currentYear = new Date().getFullYear()
-        expect(screen.getByText(`© ${currentYear} DataSmart Learning`)).toBeInTheDocument();
-    })
+  it("Copyright is displayed", () => {
+    renderWithAppProviders(<FooterNavigation />);
+    const currentYear = new Date().getFullYear();
+    expect(
+      screen.getByText(`© ${currentYear} DataSmart Learning`),
+    ).toBeInTheDocument();
+  });
 
-    it("Impressum Link is displayed and works", () => {
-        renderWithAppProviders(<FooterNavigation />)
-        const impressumLink = screen.getByRole("link", { name: "Impressum" });
-        expect(impressumLink).toBeInTheDocument();
-        expect(impressumLink).toHaveAttribute("href", "/Impressum");
-    })
+  it("Impressum Link is displayed and works", () => {
+    renderWithAppProviders(<FooterNavigation />);
+    const impressumLink = screen.getByRole("link", { name: "Impressum" });
+    expect(impressumLink).toBeInTheDocument();
+    expect(impressumLink).toHaveAttribute("href", "/Impressum");
+  });
 
-    it("Datenschutz is displayed", () => {
-        renderWithAppProviders(<FooterNavigation />)
-        const datenschutzLink = screen.getByRole("link", { name: "Datenschutz" });
-        expect(datenschutzLink).toBeInTheDocument();
-        expect(datenschutzLink).toHaveAttribute("href", "/Datenschutz");
-    })
+  it("Datenschutz is displayed", () => {
+    renderWithAppProviders(<FooterNavigation />);
+    const datenschutzLink = screen.getByRole("link", { name: "Datenschutz" });
+    expect(datenschutzLink).toBeInTheDocument();
+    expect(datenschutzLink).toHaveAttribute("href", "/Datenschutz");
+  });
 
-    it("AGBs are displayed", () => {
-        renderWithAppProviders(<FooterNavigation />)
-        const agbLink = screen.getByRole("link", { name: "AGBs und Widerrufsbelehrung" });
-        expect(agbLink).toBeInTheDocument();
-        expect(agbLink).toHaveAttribute("href", "/AGB");
-    })
-})
+  it("AGBs are displayed", () => {
+    renderWithAppProviders(<FooterNavigation />);
+    const agbLink = screen.getByRole("link", {
+      name: "AGBs und Widerrufsbelehrung",
+    });
+    expect(agbLink).toBeInTheDocument();
+    expect(agbLink).toHaveAttribute("href", "/AGB");
+  });
+});
