9ad6bd87b97be7a628f21a78680ce4b0c32297d0
Andreas Diller <AndreasDiller@AndreasDiller.onmicrosoft.com>
2025-09-30 18:47:34 +0200

    AuthContext Unit Tests

---
+++ frontend/tests/testMocks.tsx
@@ -0,0 +1,12 @@
+export const mockUser = {
+    date_joined: "2025-01-01",
+    email: "test@example.com",
+    first_name: "Test",
+    force_password_change: false,
+    full_name: "Test User",
+    user_id: 1,
+    is_active: true,
+    last_login: "2025-01-02",
+    last_name: "User",
+    username: "testuser",
+};
